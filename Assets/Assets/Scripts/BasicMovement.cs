e93401078b3d381669858d3a240a1773d657db29
Hooder <hoodernew@gmail.com>
2024-10-24 22:48:38 +1300

    Update
    Added basic all around movement and a basic sonic t pose model
    
    next will be testing roatation then a ring object

--- Assets/Assets/Scripts/BasicMovement.cs
+++ Assets/Assets/Scripts/BasicMovement.cs
@@ -8,13 +8,20 @@ public class BasicMovement : MonoBehaviour
 {
     public float BaseSpeed; // public base speed for sonic that is zero by default 
 
+    private Rigidbody rb; // calls rigidbody from object and given new data name? 
+
     void Start() 
     {
-        Rigidbody rb = GetComponent<Rigidbody>(); // calls for Rigidbody and gives a new component (I think?)
+        rb = GetComponent<Rigidbody>(); 
     }
     
     void FixedUpdate()
     {
-        
+        float moveAlongX = Input.GetAxis("Horizontal");
+        float moveAlongZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveAlongX, 0, moveAlongZ) * BaseSpeed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
     }
 }
