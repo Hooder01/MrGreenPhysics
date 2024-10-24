ec0a4e3467bf1b65491c04a58e6df7fbf4cbb26f
Hooder <hoodernew@gmail.com>
2024-10-25 12:23:57 +1300

    Update
    Focusing on the jump and boost
    
    Note:
    
    add Vector3.up

--- Assets/Assets/Scripts/BasicMovement.cs
+++ Assets/Assets/Scripts/BasicMovement.cs
@@ -7,21 +7,58 @@ using UnityEngine;
 public class BasicMovement : MonoBehaviour
 {
     public float BaseSpeed; // public base speed for sonic that is zero by default 
+    public float jumpForce;
 
     private Rigidbody rb; // calls rigidbody from object and given new data name? 
+    private float rotateSpeed;
+    private float boostSpeed = 5f;
 
     void Start() 
     {
         rb = GetComponent<Rigidbody>(); 
     }
     
-    void FixedUpdate()
+    void Movement()
     {
         float moveAlongX = Input.GetAxis("Horizontal");
         float moveAlongZ = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveAlongX, 0, moveAlongZ) * BaseSpeed * Time.deltaTime;
 
+        if (Input.GetKeyDown("right shift")) // make this hold?
+        {
+            Debug.Log("Boost active");
+
+            BaseSpeed = boostSpeed; // this works! but now how to bring it back down?
+        }
+
         rb.MovePosition(transform.position + movement);
+
+        
+    } // separated function to keep tidy
+    void Jumping()
+    {
+        bool isGrounded;
+
+        float moveAlongY = Input.GetAxis("Vertical");
+
+        Vector3 jump = new Vector3(0, moveAlongY, 0) * jumpForce * Time.deltaTime;
+
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Jumped!");
+        }
+
+        // fix this !!
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Movement(); // calls movement for use
     }
 }
