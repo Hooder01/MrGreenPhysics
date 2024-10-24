a8ed24238f77a76b58f1a00abe64da8706035d9d
Hooder <hoodernew@gmail.com>
2024-10-24 22:15:13 +1300

    Upload

---
+++ Assets/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+
+public class BasicMovement : MonoBehaviour
+{
+    public float BaseSpeed; // public base speed for sonic that is zero by default 
+
+    void Start() 
+    {
+        Rigidbody rb = GetComponent<Rigidbody>(); // calls for Rigidbody and gives a new component (I think?)
+    }
+    
+    void FixedUpdate()
+    {
+        
+    }
+}
