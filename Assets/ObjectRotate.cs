9380d2b04afa6f8b660497dd98d4d100ab599135
Hooder <hoodernew@gmail.com>
2024-10-30 00:28:54 +1300

    Still fixing rotation from this morning

--- Assets/ObjectRotate.cs
+++ Assets/ObjectRotate.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class ObjectRotate : MonoBehaviour
 {
     public float rotationSpeed;
-    private RigidBody rb;
+    private float aroundCall;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<>(RigidBody); // why can't this be found!?
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float aroundX = Input.GetAxis("Horzontal") * aroundCall;
+
+        Quaternion target = Quaternion.Euler(aroundX, 0, 0);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * rotationSpeed);
     }
 }
