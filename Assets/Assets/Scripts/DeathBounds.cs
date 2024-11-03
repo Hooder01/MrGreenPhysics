a6fe5d9697d186cee9fa2e663a6a3d8350588e92
Hooder <hoodernew@gmail.com>
2024-11-03 22:32:48 +1300

    Minor
    Added a basic DeathPlane and started messing with effects

---
+++ Assets/Assets/Scripts/DeathBounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBounds : MonoBehaviour
+{
+
+    private GameObject callPlayer; // 1!
+
+    void Start()
+    {
+        callPlayer = GameObject.Find("playerSonic"); // 2!
+    }
+
+    void OnCollisionEnter(Collision collision) // 3!
+    {
+        Debug.Log("Player Dead?");
+        Destroy(callPlayer);
+
+        // needs to respawn sonic after
+    }
+
+    // will destroy player/kill on contact (context if player goes outta bounds) 
+}
