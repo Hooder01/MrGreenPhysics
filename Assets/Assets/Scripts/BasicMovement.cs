9515c50c83222ebe99b2bbe96da0195045961259
Hooder <hoodernew@gmail.com>
2024-11-07 12:08:08 +1300

    Update
    Added some basic momentum but lots and lots of stuff to re work around.

--- Assets/Assets/Scripts/BasicMovement.cs
+++ Assets/Assets/Scripts/BasicMovement.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class jumpBallTEMP // just keeping the jump ball in here for now for the sake of my eyes
+{
+    /*public GameObject callJumpBall; // jumpball context1
+    bool isJumpBallActive; // jumpball context2 (this will check in context very often!)*/
 
+    /*callJumpBall = GameObject.Find("JumpBall"); // jumpball context3
+        Destroy(callJumpBall);*/
+}
 
 public class BasicMovement : MonoBehaviour
 {
-    public float BaseSpeed; // public base speed for sonic that is zero by default (may or may not make private in the future)
+    private float BaseSpeed = 1f; // public base speed for sonic that is zero by default (may or may not make private in the future)
+    private float acceleration = 1f;
+    private float maxCapSpeed = 130f;
 
-    private float jumpForce = 4f; // Jumping1
-    bool isInAir = false;// jumping2
-    public GameObject callJumpBall; // jumpball context1
-    bool isJumpBallActive; // jumpball context2 (this will check in context very often!)
-
+    // a public gameobject for dectecting when going down slopes!
+    
     private Rigidbody rb; // calls rigidbody from object and given new data name? 
 
-    
+    private Animator SonicAnim;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); // Grabs the rigidbody from within unity
-        isInAir = false;
+        rb = GetComponent<Rigidbody>(); 
+        SonicAnim = GetComponent<Animator>();
 
-        callJumpBall = GameObject.Find("JumpBall"); // jumpball context3
-        Destroy(callJumpBall); 
-
-        // Somehow set up the boolean with the jumpball being destroy (same applies in the if statment)
+        SonicAnim.SetTrigger("TriggerIdle"); // this is not working!!
     }
 
     void Movement()
@@ -37,6 +40,14 @@ public class BasicMovement : MonoBehaviour
 
         rb.MovePosition(transform.position + movement);
 
+        BaseSpeed += acceleration * Time.deltaTime;
+
+        if(BaseSpeed > maxCapSpeed)
+        {
+            BaseSpeed = maxCapSpeed;
+        }
+        // return to a base speed?
+
 
         if (movement != Vector3.zero) // Makes sure that movement is always using Vector3
         {
@@ -46,18 +57,6 @@ public class BasicMovement : MonoBehaviour
         // rotates Sonic to the pressed input in question
     }
   
-    void onJump()
-    {
-        if (Input.GetKeyDown("space") && isInAir == true)
-        {
-            Debug.Log("Pressed!");
-
-            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); // always make sure to add ForceMode!
-
-        }
-        
-        // this not working now??
-    }
 
     void Update()
     {
@@ -67,7 +66,5 @@ public class BasicMovement : MonoBehaviour
     void FixedUpdate()
     {
         Movement(); 
-        onJump();
-        
     }
 }
