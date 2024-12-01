6998bc25c24fff5fa7e69800c717b169a9596cdf
Hooder <hoodernew@gmail.com>
2024-12-01 20:11:36 +1300

    New Build
    Starting the new Sonic tomorrow!

---
+++ Assets/NEWScripts/GlobalValues.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalValues : MonoBehaviour
+{
+    //
+    [Header("On_Ground_Control")]
+    float baseSpeed;
+    float acceleration_speed;
+    float deceleration_speed;
+    float top_speed;
+    //
+    
+}
